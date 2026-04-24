316921b0a076c2b3d8ce49cd89ff21d738f40903
Carter <beauduke2014@gmail.com>
2026-04-23 18:56:55 -0600

    Finishing up prep 1 and prep 2 assignments without stretch on prep 2

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,57 @@
+/*
+Author: Carter Weber
+
+Sources:
+    Class = 1
+*/
+
 using System;
+using System.Reflection.Metadata.Ecma335;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        string cdw_firstName = cdw_getFirstName();
+        string cdw_lastName = cdw_getLastName();
+        Console.WriteLine($"Your name is {cdw_lastName}, {cdw_firstName} {cdw_lastName}");
+    }
+
+    static string cdw_getFirstName()
+    {
+        string prompt = "What is your first name?";
+        string cdw_firstName = getString(prompt);
+        return cdw_firstName;
+    }
+
+    static string cdw_getLastName()
+    {
+        string prompt = "What is your last name?";
+        string cdw_lastName = getString(prompt);
+        return cdw_lastName;
+    }
+
+//1
+    static string getString(string Prompt)
+    {
+        string returnValue ="";
+        bool wac_flag = true;
+        while(wac_flag)
+        {
+            try
+            {
+                Console.WriteLine(Prompt);
+                returnValue = Console.ReadLine();
+                if (string.IsNullOrEmpty(returnValue)==true)
+                {
+                    throw new Exception();
+                }
+                wac_flag = false;
+            } catch(Exception)
+            {
+                Console.WriteLine("Value is not acceptable, please enter a valid name.");
+            }
+        }
+        return returnValue;
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,73 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        float cdw_gradePercentage = cdw_getGradePercentage();
+        string cdw_gradeLetter = cdw_getGradeLetter(cdw_gradePercentage);
+        string cdw_pass = cdw_checkPass(cdw_gradePercentage);
+
+        Console.WriteLine($"Your final Grade is {cdw_gradeLetter} with a {cdw_gradePercentage}%, so you {cdw_pass} this class.");
+
+    }
+
+    static float cdw_getGradePercentage()
+    {
+        float cdw_gradePercentage = toFloat("Please enter your Grade Percentage.");
+        return cdw_gradePercentage;
+    }
+    
+    static string cdw_getGradeLetter(float cdw_gradePercentage)
+    {
+        string cdw_gradeLetter = "";
+        if (cdw_gradePercentage > 90)
+        {
+            cdw_gradeLetter = "A";
+        }
+        else if (cdw_gradePercentage > 80)
+        {
+            cdw_gradeLetter = "B";
+        }
+        else if (cdw_gradePercentage > 70)
+        {
+            cdw_gradeLetter = "C";
+        }
+        else if (cdw_gradePercentage > 60)
+        {
+            cdw_gradeLetter = "D";
+        }
+        else
+        {
+            cdw_gradeLetter = "F";
+        }
+        return cdw_gradeLetter;
+    }
+    
+    static string cdw_checkPass(float cdw_gradePercentage)
+    {
+        string cdw_pass = "Fail";
+        if (cdw_gradePercentage > 70)
+        {
+            cdw_pass = "Pass";
+        }
+        return cdw_pass;
+    }
+
+    static float toFloat(string Prompt)
+    {
+        float returnValue = 0;
+        bool wac_flag = true;
+        while (wac_flag)
+        {
+            try
+            {
+                Console.WriteLine(Prompt);
+                string wacUserInputStr = Console.ReadLine();
+                returnValue = float.Parse(wacUserInputStr);
+                wac_flag = false;
+            } catch (Exception e)
+            {
+                Console.WriteLine($"Value is not acceptable, please enter a valid number. {e}");
+            }
+        }
+        return returnValue;
     }
 }
